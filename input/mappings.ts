879de7014f3b8f14a2be1e49d3f985d8f97f6cac
ianshade <zegzulakrzysztof@gmail.com>
2020-02-12 08:55:29 +0100

    Mappings

--- input/mappings.ts
+++ input/mappings.ts
@@ -1,31 +1,59 @@
-import { DeviceType, MappingVMix, MappingCasparCG, MappingAtem, MappingAtemType } from 'timeline-state-resolver'
+import { DeviceType, MappingVMixType, MappingCasparCG, MappingAtem, MappingAtemType } from 'timeline-state-resolver'
 import { TSRInput } from '../src'
 import { literal } from 'timeline-state-resolver/dist/devices/device'
+import { MappingVMixAny } from 'timeline-state-resolver/dist/types/src'
 
 export const input: TSRInput = {
 	mappings: {
-		'vmixLayer0': literal<MappingVMix>({
+		// 'vmixLayer0': literal<MappingVMix>({
+		// 	device: DeviceType.VMIX,
+		// 	deviceId: 'vmix0',
+		// }),
+		// 'vmixLayer1': literal<MappingVMix>({
+		// 	device: DeviceType.VMIX,
+		// 	deviceId: 'vmix0',
+		// }),
+		// 'vmixLayer2': literal<MappingVMix>({
+		// 	device: DeviceType.VMIX,
+		// 	deviceId: 'vmix0',
+		// }),
+		'vmix_overlay0': literal<MappingVMixAny>({
 			device: DeviceType.VMIX,
+			mappingType: MappingVMixType.Overlay,
+			index: 1,
 			deviceId: 'vmix0',
 		}),
-		'vmixLayer1': literal<MappingVMix>({
+		'vmix_program0': literal<MappingVMixAny>({
 			device: DeviceType.VMIX,
+			mappingType: MappingVMixType.Program,
 			deviceId: 'vmix0',
 		}),
-		'vmixLayer2': literal<MappingVMix>({
+		'vmix_program1': literal<MappingVMixAny>({
 			device: DeviceType.VMIX,
+			mappingType: MappingVMixType.Program,
+			index: 2,
 			deviceId: 'vmix0',
 		}),
-		'vmixLayer3': literal<MappingVMix>({
+		'avmix_media0_audio': literal<MappingVMixAny>({
 			device: DeviceType.VMIX,
+			mappingType: MappingVMixType.AudioChannel,
+			inputLayer: 'vmix_media0',
 			deviceId: 'vmix0',
 		}),
-		'vmixLayer4': literal<MappingVMix>({
+		'vmix_media1_audio': literal<MappingVMixAny>({
 			device: DeviceType.VMIX,
+			mappingType: MappingVMixType.AudioChannel,
+			inputLayer: 'vmix_media1',
 			deviceId: 'vmix0',
 		}),
-		'vmixLayer5': literal<MappingVMix>({
+		'vmix_media0': literal<MappingVMixAny>({
 			device: DeviceType.VMIX,
+			mappingType: MappingVMixType.Input,
+			deviceId: 'vmix0',
+		}),
+		'vmix_media1': literal<MappingVMixAny>({
+			device: DeviceType.VMIX,
+			mappingType: MappingVMixType.Input,
 			deviceId: 'vmix0',
 		}),
 		// 'casparLayer0': literal<MappingCasparCG>({
--- input/timelinevmix.ts
+++ input/timelinevmix.ts
@@ -10,7 +10,7 @@ export const input: TSRInput = {
 			enable: {
 				start: Date.now()+3000,
 			},
-			layer: 'vmixLayer0',
+			layer: 'vmix_program0',
 			content: {
 				deviceType: DeviceType.VMIX,
 				type: TimelineContentTypeVMix.PROGRAM,
@@ -24,12 +24,11 @@ export const input: TSRInput = {
 				start: Date.now()+8000,
 				duration: 5000
 			},
-			layer: 'vmixLayer0',
+			layer: 'vmix_program1',
 			content: {
 				deviceType: DeviceType.VMIX,
 				type: TimelineContentTypeVMix.PROGRAM,
-				input: 4,
-				mix: 2,
+				input: 2,
 				transition: { effect:VMixTransitionType.CubeZoom, duration: 2000 }
 			}
 		},
@@ -39,12 +38,12 @@ export const input: TSRInput = {
 				start: Date.now()+6000,
 				duration: 20000
 			},
-			layer: 'vmixLayer2',
+			layer: 'vmix_media0',
 			content: {
 				deviceType: DeviceType.VMIX,
 				type: TimelineContentTypeVMix.INPUT,
 				inputType: VMixInputType.Video,
-				input: 'C:/Users/krzyc/Downloads/Audio_Video Sync Test 1080P60.mp4',
+				filePath: 'C:/Users/krzyc/Downloads/Audio_Video Sync Test 1080P60.mp4',
 				playing: false,
 				loop: true,
 				seek: 10000
@@ -66,57 +65,21 @@ export const input: TSRInput = {
 			]
 		},
 		{
-			id: 'inputOverlayOverwrite',
-			enable: {
-				start: Date.now()+6000,
-				duration: 20000
-			},
-			layer: 'vmixLayer5',
-			content: {
-				deviceType: DeviceType.VMIX,
-				type: TimelineContentTypeVMix.INPUT,
-				inputType: VMixInputType.Video,
-				input: 'C:/Users/krzyc/Downloads/Audio_Video Sync Test 1080P60.mp4',
-				overlays: {
-					2: 1
-				}
-			}
-		},
-		{
 			id: 'image',
 			enable: {
 				start: Date.now()+6000,
 				duration: 12000
 			},
-			layer: 'vmixLayer3',
+			layer: 'vmix_media1',
 			content: {
 				deviceType: DeviceType.VMIX,
 				type: TimelineContentTypeVMix.INPUT,
 				inputType: VMixInputType.Image,
-				input: 'G:/qt projects/LivemindSignalGenerator/SignalGenerator/images/patterns/ramp.png',
+				filePath: 'G:/qt projects/LivemindSignalGenerator/SignalGenerator/images/patterns/ramp.png',
 				transform: {
 					zoom: 0.5,
 					panX: 0.3,
 					panY: 0.2,
-					alpha: 128
-				}
-			}
-		},
-		{
-			id: 'imageTrOverwrite',
-			enable: {
-				start: Date.now()+12000,
-				duration: 3000
-			},
-			layer: 'vmixLayer4',
-			content: {
-				deviceType: DeviceType.VMIX,
-				type: TimelineContentTypeVMix.INPUT,
-				input: 'G:/qt projects/LivemindSignalGenerator/SignalGenerator/images/patterns/ramp.png',
-				transform: {
-					zoom: 2,
-					panX: -0.3,
-					panY: -0.2,
 					alpha: 128
 				}
 			}
@@ -127,11 +90,10 @@ export const input: TSRInput = {
 				start: Date.now()+7000,
 				duration: 10000
 			},
-			layer: 'vmixLayer0',
+			layer: 'vmix_program0',
 			content: {
 				deviceType: DeviceType.VMIX,
 				type: TimelineContentTypeVMix.PROGRAM,
-				//input: 'C:/Users/krzyc/Downloads/Audio_Video Sync Test 1080P60.mp4'
 				inputLayer: 'vmix_media0'
 			}
 		},
@@ -141,11 +103,10 @@ export const input: TSRInput = {
 				start: Date.now()+6000,
 				duration: 8000
 			},
-			layer: 'vmixLayer1',
+			layer: 'avmix_media0_audio',
 			content: {
 				deviceType: DeviceType.VMIX,
 				type: TimelineContentTypeVMix.AUDIO,
-				input: 'C:/Users/krzyc/Downloads/Audio_Video Sync Test 1080P60.mp4',
 				volume: 60,
 				balance: -0.5,
 				audioAuto: false,
@@ -160,13 +121,62 @@ export const input: TSRInput = {
 				start: Date.now()+15000,
 				duration: 10000
 			},
-			layer: 'vmixLayer0',
+			layer: 'vmix_program0',
 			content: {
 				deviceType: DeviceType.VMIX,
 				type: TimelineContentTypeVMix.PROGRAM,
 				input: 1
 			}
 		},
+		{
+			id: 'overlay',
+			enable: {
+				start: Date.now()+1000,
+				duration: 2000
+			},
+			layer: 'vmix_overlay0',
+			content: {
+				deviceType: DeviceType.VMIX,
+				type: TimelineContentTypeVMix.OVERLAY,
+				input: 5
+			}
+		},
+		// {
+		// 	id: 'inputOverlayOverwrite',
+		// 	enable: {
+		// 		start: Date.now()+6000,
+		// 		duration: 20000
+		// 	},
+		// 	layer: 'vmixLayer5',
+		// 	content: {
+		// 		deviceType: DeviceType.VMIX,
+		// 		type: TimelineContentTypeVMix.INPUT,
+		// 		inputType: VMixInputType.Video,
+		// 		input: 'C:/Users/krzyc/Downloads/Audio_Video Sync Test 1080P60.mp4',
+		// 		overlays: {
+		// 			2: 1
+		// 		}
+		// 	}
+		// },
+		// {
+		// 	id: 'imageTrOverwrite',
+		// 	enable: {
+		// 		start: Date.now()+12000,
+		// 		duration: 3000
+		// 	},
+		// 	layer: 'vmixLayer4',
+		// 	content: {
+		// 		deviceType: DeviceType.VMIX,
+		// 		type: TimelineContentTypeVMix.INPUT,
+		// 		input: 'G:/qt projects/LivemindSignalGenerator/SignalGenerator/images/patterns/ramp.png',
+		// 		transform: {
+		// 			zoom: 2,
+		// 			panX: -0.3,
+		// 			panY: -0.2,
+		// 			alpha: 128
+		// 		}
+		// 	}
+		// },
 
 		// {
 		// 	id: 'program02',
@@ -267,20 +277,6 @@ export const input: TSRInput = {
 		// 		deviceType: DeviceType.VMIX,
 		// 		type: TimelineContentTypeVMix.FADER,
 		// 		position: 50
-		// 	}
-		// },
-		// {
-		// 	id: 'overlay',
-		// 	enable: {
-		// 		start: Date.now(),
-		// 		duration: 12000
-		// 	},
-		// 	layer: 'vmixLayer3',
-		// 	content: {
-		// 		deviceType: DeviceType.VMIX,
-		// 		type: TimelineContentTypeVMix.OVERLAY,
-		// 		overlay: 3,
-		// 		input: 5
 		// 	}
 		// },
 		// {
